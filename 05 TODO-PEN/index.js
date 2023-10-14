15d9cf4c6270bc24a0fa88df6b30c3237e87a7ed
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-10-14 23:21:31 +0545

    ~minor changes

--- 05 TODO-PEN/index.js
+++ 05 TODO-PEN/index.js
@@ -8,4 +8,4 @@ app.use(express.json());
 
 app.use('/',routes)
 
-app.listen(3000, () => console.log('http://localhost:3000'))
+app.listen(3000, () => console.log('http://localhost:3000 \nBetter open API testing tool like postman, insomnia or of your preference'))
