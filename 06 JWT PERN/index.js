17cb1ed629886dd06f39a3b83d06cce2d379c9af
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-10-16 10:49:51 +0545

    dashboard middleware

--- 06 JWT PERN/index.js
+++ 06 JWT PERN/index.js
@@ -2,12 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
 import auth from "./routes/jwtAuth.js"
+import dashboard from "./routes/dashboard.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/auth', auth)
-
+app.use('/dashboard', dashboard)
 
 app.get('/', (req, res) => {
     res.send('Welcome')
