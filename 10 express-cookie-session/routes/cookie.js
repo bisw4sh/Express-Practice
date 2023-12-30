d3f6f32bf099d4807d987e3de896edfafea07b71
Biswash <spikeystona@gmail.com>
2023-12-30 20:27:20 +0545

    cookie routes

---
+++ 10 express-cookie-session/routes/cookie.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  setCookie,
+  checkCookie,
+  changeCookie,
+  destroyCookie,
+} from "../controller/cookie.js";
+const router = express.Router();
+
+router.get("/set", setCookie);
+router.get("/check", checkCookie);
+router.get("/change", changeCookie);
+router.get("/destroy", destroyCookie);
+
+export default router;
