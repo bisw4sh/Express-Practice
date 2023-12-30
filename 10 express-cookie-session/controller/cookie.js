22c3c056a8d118aafa4ba1c6c808d7518b1bc722
Biswash <spikeystona@gmail.com>
2023-12-30 20:27:27 +0545

    cookie controllers

---
+++ 10 express-cookie-session/controller/cookie.js
@@ -0,0 +1,30 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const setCookie = async (req, res) => {
+  if (req.cookies.data) {
+    res.send("Cookie already exists");
+  } else {
+    res.cookie("data", { token: uuidv4(), user: "Biswash" });
+    res.send("<h1>Cookie has been set</h1>");
+  }
+};
+
+export const checkCookie = async (req, res) => {
+  const { data } = req.cookies;
+  console.log(data);
+  res.send("<h1>Check Server console to check the cookie</h1>");
+};
+
+export const changeCookie = async (req, res) => {
+  const newCookie = res.cookie("data", { token: "token has been changed" });
+  console.log(newCookie);
+  res.send("<h1>Token Changed. Look at server console</h1>");
+};
+
+export const destroyCookie = async (req, res) => {
+  res.cookie.clear;
+  console.log(res.cookie?.data || "No Cookie");
+  res.send(
+    "<h1>Cookie and Token has been destroyed. Look at server Console</h1>"
+  );
+};
