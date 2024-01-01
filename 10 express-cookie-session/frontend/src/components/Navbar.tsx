ded3ebee08c8e4dc961f5ca24b86a1c4ef62611b
Biswash <spikeystona@gmail.com>
2024-01-01 12:51:02 +0545

    navbar

---
+++ 10 express-cookie-session/frontend/src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import { Outlet, NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <div className="px-8 py-4">
+      <nav className="w-full flex justify-between">
+        <div className="flex gap-4">
+          <NavLink
+            to="/"
+            className="text-teal-400 hover:text-teal-600 hover:scale-105"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/register"
+            className="text-teal-400 hover:text-teal-600 hover:scale-105"
+          >
+            Register
+          </NavLink>
+          <NavLink
+            to="/dashboard"
+            className="text-teal-400 hover:text-teal-600 hover:scale-105"
+          >
+            Dashboard
+          </NavLink>
+        </div>
+        <button className="btn">
+          <NavLink to='login'>Login | Logout</NavLink>
+        </button>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
