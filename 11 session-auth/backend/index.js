01fa72351a05137ff7758ea9427a8124a33cbf6c
Biswash <spikeystona@gmail.com>
2024-01-02 21:50:21 +0545

    fixes : removed some code

--- 11 session-auth/backend/index.js
+++ 11 session-auth/backend/index.js
@@ -3,10 +3,8 @@ import session from "express-session";
 import cookieParser from "cookie-parser";
 import loginRoute from "./routes/login.js";
 import registerRoute from "./routes/register.js";
-import logoutRoute from "./routes/register.js";
+import logoutRoute from "./routes/logout.js";
 import findUserRoute from "./routes/findUser.js";
-// import active_sessions from "./middleware/active_sessions.js";
-import req_ping from "./controllers/req_ping.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -18,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
     secret: "<secret>",
+    key: "session_id",
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -26,8 +25,6 @@ app.use(
     },
   })
 );
-
-app.use(req_ping);
 
 app.get("/", (req, res) => {
   res.send("It is working");
