5aa1a4b9b98c9f68b16ac519d6eff3785716a4d1
Biswash <spikeystona@gmail.com>
2024-01-02 21:50:41 +0545

    feat : response w logged user details

--- 11 session-auth/backend/routes/findUser.js
+++ 11 session-auth/backend/routes/findUser.js
@@ -1,13 +1,23 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  if (req.cookies["connect.sid"] === req.sessionID) {
-    console.log(req.session.data);
-    res.send(req.session.data);
-  } else {
-    res.send('nth')
-  }
+  console.log("--------------------------------------------");
+
+  console.table(req.session);
+  console.table(req.cookies);
+  console.log(`Session Data : ${req.session?.data}`);
+  console.log(`Cookie Data : ${req.cookies.data?.user}`);
+
+  console.log(`Session ID : ${req.sessionID}`);
+  console.log(
+    `Cookie ID ${cookieParser.signedCookie(req.cookies.session_id, "<secret>")}`
+  );
+  console.log("--------------------------------------------");
+
+  res.send(await req.session.data);
 });
 
 export default router;
