5076a36801bc5d3dc6aef5485c5e0f62e25e5bcf
Biswash <spikeystona@gmail.com>
2024-01-02 21:48:23 +0545

    config & fixes

--- 11 session-auth/backend/controllers/req_ping.js
+++
@@ -1,9 +0,0 @@
-const req_ping = async (req, res) => {
-  if (req.cookies["connect.sid"]) {
-    console.log(req.cookies["connect.sid"]);
-  } else {
-    console.log(`Has no cookie`);
-  }
-};
-
-export default req_ping;
