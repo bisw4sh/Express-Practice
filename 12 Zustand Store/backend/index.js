7392d4acad0cc4e0bfd6208b0c128e9e0286cc6a
Biswash Dhungana <spikeystona@gmail.com>
2024-01-20 21:50:33 +0545

    product data

--- 12 Zustand Store/backend/index.js
+++ 12 Zustand Store/backend/index.js
@@ -1,10 +1,15 @@
 import express from "express";
+import { products } from "./data/products.js";
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
 });
 
 app.listen(8080, () => console.log(`Server runnning @ http://localhost:8080`));
