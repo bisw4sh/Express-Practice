a0a1ea4ec79b35e54459bcb7c0b2752cedff78d1
Biswash Dhungana <spikeystona@gmail.com>
2024-03-29 09:57:22 +0545

    docm

--- 11 session-auth/backend/routes/findUser.js
+++ 11 session-auth/backend/routes/findUser.js
@@ -5,13 +5,11 @@ router.get("/", async (req, res) => {
   try {
     console.log("--------------------------------------------");
     // console.table(req.session);
-    // console.log(req.session["data"]); //undefined
+    // console.log(req.session?.data); //undefined
     // console.log(req.cookies); //undefined
-    // console.log(req.cookies["session_id"]); //undefined
-    // console.log(`Session Data : ${req.session["data"]}`); //undefined
+    // console.log(req.cookies.session_id); //undefined
     console.log(`Session ID : ${req.sessionID}`); //this shows
     console.log("--------------------------------------------");
-    // let retVal = req.session["data"] ? req.session["data"] : "anon";
     res.send(req.session?.data);
   } catch (error) {
     console.log("There must have been an error here");
