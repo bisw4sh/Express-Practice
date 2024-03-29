8f2fe5021001452872c1cc4c05c3e8b2ff558cd1
Biswash Dhungana <spikeystona@gmail.com>
2024-03-29 09:50:45 +0545

    minor fix

--- 11 session-auth/backend/routes/findUser.js
+++ 11 session-auth/backend/routes/findUser.js
@@ -1,6 +1,4 @@
 import express from "express";
-// import cookieParser from "cookie-parser";
-
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -9,14 +7,14 @@ router.get("/", async (req, res) => {
     // console.table(req.session);
     // console.log(req.session["data"]); //undefined
     // console.log(req.cookies); //undefined
-    console.log(req.cookies.session_id); //undefined
+    // console.log(req.cookies["session_id"]); //undefined
     // console.log(`Session Data : ${req.session["data"]}`); //undefined
     console.log(`Session ID : ${req.sessionID}`); //this shows
     console.log("--------------------------------------------");
     // let retVal = req.session["data"] ? req.session["data"] : "anon";
-    res.send("no idea");
+    res.send(req.session?.data);
   } catch (error) {
-    console.log("There must have been error here");
+    console.log("There must have been an error here");
     res.status(500).send(error);
   }
 });
--- 11 session-auth/backend/routes/private.js
+++ 11 session-auth/backend/routes/private.js
@@ -3,13 +3,10 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  console.log("it was here ");
-
   if (req.session.data === "biswash") {
     res.send(`The private data is only being sent to ${req.session.data}`);
-    console.log("Yeha biswash matra milxa hai");
   } else {
-    res.send("yesto tah garna bhayena ni");
+    res.send(`redirect`);
   }
 });
 
